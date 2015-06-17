53f87520524ddc57af887c19ba3684711c86d71e
Frederic Mokren <fredericmokren@Frederics-MacBook-Air.local>
2015-06-16 21:50:47 -0700

    Initial Commit

---
+++ YABA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YABA
+//
+//  Created by Frederic Mokren on 6/16/15.
+//  Copyright (c) 2015 Frederic Mokren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
